782f5886e33e015708d50cc958952e2871621121
ggsdatabase <sionbussines21@gmail.com>
2025-07-13 21:16:44 -0300

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 export const firebaseConfig = {
   apiKey: "AIzaSyCr0SM_u0c4aEG_tT_CXWLcvd6alZJT44c",
   authDomain: "siondata22.firebaseapp.com",
@@ -10,9 +6,3 @@ export const firebaseConfig = {
   messagingSenderId: "946379205241",
   appId: "1:946379205241:web:0fe04a6246518451ee42a8"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
