2f6324093c29c556cd14fe6f9a79a199719c6d48
ggsdatabase <sionbussines21@gmail.com>
2025-07-14 13:38:36 -0300

    firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,15 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCr0SM_u0c4aEG_tT_CXWLcvd6alZJT44c",
-  authDomain: "siondata22.firebaseapp.com",
-  projectId: "siondata22",
-  storageBucket: "siondata22.appspot.com",
-  messagingSenderId: "946379205241",
-  appId: "1:946379205241:web:0fe04a6246518451ee42a8"
-};
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+
+const firebaseConfig = {
+      apiKey: "AIzaSyCr0SM_u0c4aEG_tT_CXWLcvd6alZJT44c",
+      authDomain: "siondata22.firebaseapp.com",
+      projectId: "siondata22",
+      storageBucket: "siondata22.appspot.com",
+      messagingSenderId: "946379205241",
+      appId: "1:946379205241:web:0fe04a6246518451ee42a8",
+      measurementId: "G-EX6YDJYS1N"
+    };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
